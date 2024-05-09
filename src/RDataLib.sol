a03a9ee40e8038a4bad562ac74d911f80ffc833e
zeroknots <zeroknots@protonmail.com>
2024-05-09 09:11:03 +0700

    chore: update foundry toml to adhere to fmt

--- src/RDataLib.sol
+++ src/RDataLib.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
-import {RDataStorage} from "./RDataStorage.sol";
+import { RDataStorage } from "./RDataStorage.sol";
 
 /**
  * @title RData
