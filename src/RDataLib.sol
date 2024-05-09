8ddbfbbf000af4c39405893323a5a0226508fdcc
zeroknots <zeroknots@protonmail.com>
2024-05-09 08:58:54 +0700

    splitting into two files for better readability

--- src/RDataLib.sol
+++ src/RDataLib.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
+import {RDataStorage} from "./RDataStorage.sol";
+
+/**
+ * @author rhinestone | zeroknots.eth
+ */
 library RData {
     struct Bytes32 {
         bytes32 b32;
@@ -162,99 +167,5 @@ library RData {
             _slot := str.slot
         }
         value = storageContract.getString(_slot);
-    }
-}
-
-contract RDataStorage {
-    mapping(bytes32 => mapping(address account => string)) internal stringStorage;
-    mapping(bytes32 => mapping(address account => bytes)) internal bytesStorage;
-    mapping(bytes32 => mapping(address account => uint256)) internal uintStorage;
-    mapping(bytes32 => mapping(address account => int256)) internal intStorage;
-    mapping(bytes32 => mapping(address account => address)) internal addressStorage;
-    mapping(bytes32 => mapping(address account => bool)) internal booleanStorage;
-    mapping(bytes32 => mapping(address account => bytes32)) internal bytes32Storage;
-
-    function getAddress(bytes32 _key) external view returns (address r) {
-        return addressStorage[_key][msg.sender];
-    }
-
-    function getUint(bytes32 _key) external view returns (uint256 r) {
-        return uintStorage[_key][msg.sender];
-    }
-
-    function getString(bytes32 _key) external view returns (string memory) {
-        return stringStorage[_key][msg.sender];
-    }
-
-    function getBytes(bytes32 _key) external view returns (bytes memory) {
-        return bytesStorage[_key][msg.sender];
-    }
-
-    function getBool(bytes32 _key) external view returns (bool r) {
-        return booleanStorage[_key][msg.sender];
-    }
-
-    function getInt(bytes32 _key) external view returns (int256 r) {
-        return intStorage[_key][msg.sender];
-    }
-
-    function getBytes32(bytes32 _key) external view returns (bytes32 r) {
-        return bytes32Storage[_key][msg.sender];
-    }
-
-    function setAddress(bytes32 _key, address _value) external {
-        addressStorage[_key][msg.sender] = _value;
-    }
-
-    function setUint(bytes32 _key, uint256 _value) external {
-        uintStorage[_key][msg.sender] = _value;
-    }
-
-    function setString(bytes32 _key, string calldata _value) external {
-        stringStorage[_key][msg.sender] = _value;
-    }
-
-    function setBytes(bytes32 _key, bytes calldata _value) external {
-        bytesStorage[_key][msg.sender] = _value;
-    }
-
-    function setBool(bytes32 _key, bool _value) external {
-        booleanStorage[_key][msg.sender] = _value;
-    }
-
-    function setInt(bytes32 _key, int256 _value) external {
-        intStorage[_key][msg.sender] = _value;
-    }
-
-    function setBytes32(bytes32 _key, bytes32 _value) external {
-        bytes32Storage[_key][msg.sender] = _value;
-    }
-
-    function deleteAddress(bytes32 _key) external {
-        delete addressStorage[_key][msg.sender];
-    }
-
-    function deleteUint(bytes32 _key) external {
-        delete uintStorage[_key][msg.sender];
-    }
-
-    function deleteString(bytes32 _key) external {
-        delete stringStorage[_key][msg.sender];
-    }
-
-    function deleteBytes(bytes32 _key) external {
-        delete bytesStorage[_key][msg.sender];
-    }
-
-    function deleteBool(bytes32 _key) external {
-        delete booleanStorage[_key][msg.sender];
-    }
-
-    function deleteInt(bytes32 _key) external {
-        delete intStorage[_key][msg.sender];
-    }
-
-    function deleteBytes32(bytes32 _key) external {
-        delete bytes32Storage[_key][msg.sender];
     }
 }
--- test/Storage.t.sol
+++ test/Storage.t.sol
@@ -2,7 +2,8 @@
 pragma solidity >=0.8.0;
 
 import "forge-std/Test.sol";
-import "src/EIP7702Storage.sol";
+import "src/RDataStorage.sol";
+import "src/RdataLib.sol";
 
 contract StorageTest is Test {
     using RData for RData.Address;
@@ -10,7 +11,8 @@ contract StorageTest is Test {
 
     RDataStorage storageContract;
 
-    mapping(address module => RData.Address) internal installedModule;
+    mapping(address module => RData.Address) internal addresses;
+    mapping(address module => RData.Bytes32) internal bytes32s;
 
     function setUp() public virtual {
         storageContract = new RDataStorage();
@@ -18,11 +20,20 @@ contract StorageTest is Test {
         vm.etch(address(RData.storageContract), address(storageContract).code);
     }
 
-    function test_store(address value) public {
-        address module = makeAddr("module");
-        installedModule[module].store(value);
-        address _value = installedModule[module].load();
+    function test_address(address value) public returns (address _value) {
+        address key = makeAddr("key");
+        addresses[key].store(value);
+        _value = addresses[key].load();
 
         assertEq(value, _value);
     }
+
+    function test_bytes32(bytes32 value) public returns (bytes32 _value) {
+        address key = makeAddr("key");
+        bytes32s[key].store(value);
+        _value = bytes32s[key].load();
+
+        assertEq(value, _value);
+    }
+
 }
